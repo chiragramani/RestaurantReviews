967daa641d00673816e109abc1ac6ce624577019
Chirag Ramani <chirag.ramani7@gmail.com>
2019-02-20 18:30:30 +0530

    Configured service worker and cache..

--- sw.js
+++ sw.js
@@ -1,20 +1,51 @@
 const validCacheName = "restaurant-reviews-crg";
 
-self.addEventListener('install', function(event) {
-    const urlsToCache = [
-        '/',
-        'css/styles.css',
-        'data/restaurants.json',
-        'img/*',
-        'js/*',
-        'index.html',
-        'restaurant.html'
-    ];
-    event.waitUntil(
-        caches.open(validCacheName).then(cache => cache.addAll())
-    );
-})
+self.addEventListener("install", function(event) {
+  const urlsToCache = [
+    "/",
+    "css/styles.css",
+    "data/restaurants.json",
+    "img/*",
+    "js/*",
+    "index.html",
+    "restaurant.html"
+  ];
+  event.waitUntil(
+    caches
+      .open(validCacheName)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(function(error) {
+        console.log(error);
+      })
+  );
+});
 
-self.addEventListener('fetch', function(event) {
- console.log('a');
+self.addEventListener("fetch", function(event) {
+  event.waitUntil(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      fetch(event.request).then(response => {
+        caches.open(validCacheName).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != validCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
