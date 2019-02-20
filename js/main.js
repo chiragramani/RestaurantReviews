7d5dc17abc4c4ccb59c0aab4dd4c0e1a8353ee30
Chirag Ramani <chirag.ramani7@gmail.com>
2019-02-20 23:45:52 +0530

    Added language info and alt info

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt',`${restaurant.name} in ${restaurant.neighborhood}`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
